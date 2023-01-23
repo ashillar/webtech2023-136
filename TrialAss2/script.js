5e26ea722a58783dd234ed80142bd987dfbd9c67
washhhatsup <74905245+washhhatsup@users.noreply.github.com>
2023-01-23 15:29:08 +0100

    search for author

--- TrialAss2/script.js
+++ TrialAss2/script.js
@@ -65,3 +65,25 @@ $('#db-reset').submit(function (e) {
   });
 });
 
+function tableSearch() {
+    let input, filter, table, tr, td, txtValue;
+
+    //Intialising Variables
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("data-output");
+    tr = table.getElementsByTagName("tr");
+
+    for (let i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+}
