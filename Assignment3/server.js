89a629ae7eeb15c21335a24bb0b5cb1255a6490b
washhhatsup <74905245+washhhatsup@users.noreply.github.com>
2023-01-31 09:56:11 +0100

    specify response500

--- Assignment3/server.js
+++ Assignment3/server.js
@@ -113,7 +113,7 @@ app.delete("/photos/:id", (req, res) => {
   const query = `DELETE FROM gallery WHERE id = ?`;
   db.run(query, [id], (err) => {
     if (err) {
-      return console.error(err.message);
+      return res.status(500).json({ error: err.message });
     }
     res.sendStatus(200);
   });
